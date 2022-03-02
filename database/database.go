6c78f973f1e20a6735d4d43454ac6f708893a3b4
Junghoon Noh <jungnoh.dev@gmail.com>
2022-03-02 23:41:05 +0900

    Remove unnecessary code

--- database/database.go
+++ database/database.go
@@ -1,56 +1,26 @@
 package database
 
 import (
-	"context"
-	"fmt"
-
 	"github.com/jungnoh/mora/common"
 	"github.com/jungnoh/mora/database/command"
 	"github.com/jungnoh/mora/database/storage"
 	"github.com/jungnoh/mora/database/util"
 	"github.com/jungnoh/mora/page"
 	"github.com/pkg/errors"
-	"github.com/rs/zerolog/log"
 )
 
 type Database struct {
-	config          util.Config
-	Storage         *storage.Storage
-	evcitedNotiChan chan *page.Page
-	ctx             context.Context
-	ctxCancel       context.CancelFunc
+	config  util.Config
+	Storage *storage.Storage
 }
 
 func NewDatabase(config util.Config) (*Database, error) {
 	db := Database{}
-	db.ctx, db.ctxCancel = context.WithCancel(context.Background())
 	db.config = config
 	db.Storage = storage.NewStorage(&db.config)
-	db.evcitedNotiChan = make(chan *page.Page)
 
-	go db.evict()
 	return &db, nil
 }
-
-func (d *Database) evict() {
-	for {
-		select {
-		case <-d.ctx.Done():
-			return
-		case pg := <-d.evcitedNotiChan:
-			err := d.execEvict(pg)
-			if err != nil {
-				log.Panic().Err(err).Msg("Eviction failed!")
-			}
-		}
-	}
-}
-
-func (d *Database) execEvict(pg *page.Page) error {
-	fmt.Println(pg.UniqueKey())
-	return nil
-}
-
 func (d *Database) Execute(commands []command.CommandContent) error {
 	accessor, err := d.Storage.Access()
 	if err != nil {
